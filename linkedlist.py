ad34081b61c369345d0ea8f095c7b7a17ce23f62
William McKeon <islanderrobotics@gmail.com>
2021-07-06 19:03:21 -0400

    just switching something

--- linkedlist.py
+++ linkedlist.py
@@ -36,6 +36,48 @@ class LinkedList:
         while(temp is not None):
             print(f"{temp.symbol}, {temp.price},{temp.current_percentage},{temp.overall_percentage}")
             temp = temp.next
+    def Size(self):
+        return self.size_
+    def contains(self, symbol):
+        temp = self.head
+        while(temp is not None):
+            if (temp.symbol == symbol):
+                return True
+            temp = temp.next
+        return False
+    def insert(self,price,current_percentage,overall_percentage,symbol,index):
+        if index == 0:
+            self.PushFront(price,current_percentage,overall_percentage,symbol)
+            return
+        if index >= self.size_:
+            self.PushBack(price,current_percentage,overall_percentage,symbol)
+            temp = self.head
+            curr_index = 0
+        while(curr_index != index-1):
+            temp = temp.next
+            curr_index+=1
+        new_node = Node(next = temp.next, price= price,current_percentage=current_percentage,overall_percentage=overall_percentage,symbol=symbol)
+        temp.next = new_node
+        self.size_+=1
+
+    def delete(self, symbol):
+        current = self.head
+        previous = Node
+        if current.symbol==symbol:
+            previous=current
+            self.head = current.next
+            del previous
+            self.size_-=1
+        else:
+            while(current.symbol != symbol and current is None):
+                previous=current
+                current = current.next
+
+        if(current):
+            if current == self.tail:
+                previous.next = current.next
+            del current
+        self.size_+=1
 
 if __name__ == "__main__":
     data = LinkedList()
@@ -44,5 +86,13 @@ if __name__ == "__main__":
     print("the new list is ")
     data.PushBack(432,6543,6,"will")
     data.PrintValues()
-    data.Dynamic(725,635,25,"you")
+    data.Dynamic(725, 635, 25, "you")
+    print("the new list is ")
     data.PrintValues()
+    data.insert(725,635,25,"do", 2)
+
+    print("the new list is ")
+    data.PrintValues()
+    # data.delete("will ")
+    # print("the new list is ")
+    # data.PrintValues()
