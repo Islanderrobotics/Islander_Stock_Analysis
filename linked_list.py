3bac7e31d4eb36b127beccb5100104de9ae9aa87
William McKeon <islanderrobotics@gmail.com>
2021-07-09 20:37:08 -0400

    ready

--- linked_list.py
+++ linked_list.py
@@ -1,13 +1,12 @@
+import price
+
 
 class Node:
     '''essentialy this class is the back bone for the linked list '''
-    def __init__(self,next = None, price = None, current_percentage = None,
-                 overall_percentage = None, symbol = None):
+
+    def __init__(self,next = None, data = None):
         self.next = next
-        self.price = price
-        self.current_percentage = current_percentage
-        self.overall_percentage = overall_percentage
-        self.symbol = symbol
+        self.data = data
 
 class LinkedList:
     '''this is class will allow for the user the ability to convert
@@ -18,34 +17,35 @@ class LinkedList:
         self.head = None
         self.tail = None
         self.sizeofLinkedList =0
-    def PushFront(self,price:int,current_percentage:float,overall_percentage:float,symbol:str):
-        temp = Node(next = self.head,price = price,current_percentage = current_percentage,
-                    overall_percentage=overall_percentage, symbol=symbol)
+    def PushFront(self,data):
+        temp = Node(next = self.head,data = data)
         self.head = temp
         self.sizeofLinkedList+=1
         if (self.sizeofLinkedList==1):
             self.tail = self.head
-    def PushBack(self,price:int,current_percentage:float,overall_percentage:float,symbol:str):
-        self.tail.next = Node(price=price,current_percentage=current_percentage,overall_percentage=overall_percentage,symbol=symbol)
+    def PushBack(self,data):
+        self.tail.next = Node(data = data)
         self.tail = self.tail.next
         self.sizeofLinkedList+=1
     
-    def Dynamic(self,price,current_percentage,overall_percentage,symbol):
+    def Dynamic(self,data):
         if self.sizeofLinkedList==0:
-            self.PushFront(price,current_percentage,overall_percentage,symbol)
+            self.PushFront(data)
         else:
-            self.pushBack(price,current_percentage,overall_percentage,symbol)
+            self.PushBack(data)
     def printValues(self)->str:
-        print(self.sizeofLinkedList)
         temp: Node = self.head
         size = 0
         while(self.head is not None):
-            print(f"{self.head.symbol},{self.head.price},{self.head.overall_percentage}, {self.head.current_percentage}")
+            if (isinstance(self.head.data,dict) is False):
+                print(f"{self.head.data}")
+            else:
+                print(f"{self.head.data}")
             # if (size>=1):
             #     temp.next = self.head
             self.head = self.head.next
         self.head = temp
-    def size(self)->str:
+    def size(self)->int:
         return self.sizeofLinkedList
 
     def Contains(self,symbol:str)->bool:
@@ -56,31 +56,31 @@ class LinkedList:
             self.head = self.head.next
         self.head = temp
         return False
-    def insert(self,price:int,current_percentage:float,overall_percentage:float,symbol:str, index:int):
+    def insert(self,data,index):
         if index ==0:
-            self.PushFront(price=price,symbol=symbol,current_percentage=current_percentage,overall_percentage=overall_percentage)
+            self.PushFront(data)
             return
         if index >= self.sizeofLinkedList:
-            self.PushBack(price=price, symbol=symbol, current_percentage=current_percentage,overall_percentage=overall_percentage)
+            self.PushBack(data)
         temp :Node = self.head
         curr_index = 0
         while(curr_index != index-1):
             temp = temp.next
             curr_index+=1
-        new_node: Node = Node(next=temp.next, price=price,symbol=symbol,current_percentage=current_percentage,overall_percentage=overall_percentage)
+        new_node: Node = Node(next=temp.next, data= data)
         temp.next = new_node
         self.sizeofLinkedList+=1
-    def delete(self,symbol):
+    def deleteReg(self,symbol):
         current:Node = self.head
         previouse:Node = None
 
-        if (current.symbol == symbol):
+        if (current.data == symbol):
             previouse = current
             self.head = current.next
             del previouse
             self.sizeofLinkedList=-1
         else:
-            while(current.symbol != symbol and current is not None):
+            while(current.data != symbol and current is not None):
                 previouse = current
                 current = current.next
         if(current):
@@ -89,15 +89,35 @@ class LinkedList:
             previouse.next = current.next
             del current
             self.sizeofLinkedList-=1
-data = LinkedList()
+    def deletedict(self,key,data):
+        current: Node = self.head
+        previouse: Node = None
+
+        if (current.data[key] == data):
+            previouse = current
+            self.head = current.next
+            del previouse
+            self.sizeofLinkedList = -1
+        else:
+            while (current.data[key] != data and current is not None):
+                previouse = current
+                current = current.next
+        if (current):
+            if (current == self.tail):
+                self.tail = previouse
+            previouse.next = current.next
+            del current
+            self.sizeofLinkedList -= 1
+    def delete(self,data,dict = False, key = ""):
+        if (dict is False):
+            self.deleteReg(data)
+        else:
+            self.deletedict(key = key, data = data)
 if __name__ == "__main__":
-    data.PushFront(price=123, symbol="what",current_percentage=100, overall_percentage = 20)
-    data.printValues()
-    data.PushBack(price = 430, symbol="will",current_percentage=300, overall_percentage=432)
-    data.printValues()
-    data.Dynamic(price = 200,symbol="you",current_percentage=54,overall_percentage=45)
-    data.printValues()
-    data.insert(price = 200,symbol="do",current_percentage=54,overall_percentage=45,index=2)
-    data.printValues()
-    data.delete("do")
+    data = LinkedList()
+    data.PushFront(data = {"price":123, "current percentage":43})
+    data.PushFront(data = {"price":89, "current percentage":79})
+    data.PushBack(data = {"price":54, "current percentage":89})
+    data.Dynamic(data = {"price":874, "current":42})
+    data.delete(data = 89, dict = True, key = "current percentage")
     data.printValues()
