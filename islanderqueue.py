57f36bd1a6197c9bb5e3f15d89304b3d2a7dd4bd
Islanderrobotics <75331800+Islanderrobotics@users.noreply.github.com>
2021-07-14 18:56:20 -0400

    video on BS4 is done

--- islanderqueue.py
+++ islanderqueue.py
@@ -7,12 +7,19 @@ class IslanderQueue(ll,Heap):
         ll.__init__(self)
         Heap.__init__(self)
         self.priority = priority
+        self.count = 0
     def Empty(self):
         if self.size() == 0:
             return True
         return False
+    def Size(self):
+        # if (self.priority):
+        #     return self._Size(self.root)
+        # else:
+            return self.size()
     def Push(self,data,key = None):
         if (self.priority is True):
+            self.count += 1
             self.Insert(data,key = key)
         else:
             self.Dynamic(data = data)
@@ -21,9 +28,9 @@ class IslanderQueue(ll,Heap):
             return self.root
         else:
             return self.head.data
-    def Pop(self):
+    def Pop(self,key = None):
         if self.priority is True:
-            self.RemoveMax()
+            self.RemoveMax(key = key)
         else:
             self.head =self.head.next
 if __name__ == '__main__':
@@ -32,6 +39,15 @@ if __name__ == '__main__':
     data.Push(data={"price":0.14, "current":"will"},key = "price")
     print(data._Size(data.root))
     data.Push(data = {"price":0.133,"current":"you"},key = "price")
-    # print(data.size())
-    # print(data.Top())
-    data.PreOrder()
+    data.Heapify(key="price")
+    while (data.root is not None):
+        data.Dynamic(data.root.data)
+        if (data.root.left is not None or data.root.right is not None):
+            data.RemoveMax(key="price")
+        if (data.root.left is None and data.root.right is None):
+            data.Dynamic(data.root.data)
+            break
+
+    while(data.head is not None):
+        print(data.head.data)
+        data.head = data.head.next
