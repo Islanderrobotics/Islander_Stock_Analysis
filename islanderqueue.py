7a7bfcf855e413303131ac0b4b5e39737123c1ec
Islanderrobotics <75331800+Islanderrobotics@users.noreply.github.com>
2021-07-18 23:20:21 -0400

    Update islanderqueue.py

--- islanderqueue.py
+++ islanderqueue.py
@@ -1,19 +1,15 @@
-import linkedlist
+
 from linkedlist import LinkedList as ll
-from linkedlist import Node
 class IslanderQueue(ll):
-    def __init__(self, priority = False):
+    def __init__(self):
         ll.__init__(self)
-        self.priority = priority
+        
     def Empty(self):
         if self.size() == 0:
             return True
         return False
     def Push(self,data,key = None):
-        if (self.priority):
-            self.Priority(index = data,key = key)
-        else:
-            self.Dynamic(data = data)
+        self.Dynamic(data = data)
     def Top(self, pop=False):
         data = self.head.data
         if (pop is True):
@@ -21,25 +17,6 @@ class IslanderQueue(ll):
         return data
     def Pop(self):
         self.head =self.head.next
-    def Priority(self, index, key=None):
-        if self.sizeofLinkedList == 0:
-            self.PushFront(index)
-            return
-        temp:Node = self.head
-        if (isinstance(self.head.data, dict) is False):
-
-            while (temp is not None and temp.data < index):
-                temp = temp.next
-        else:
-            print("yes")
-            while (temp is not None and temp.data[key] < index[key]):
-                temp = temp.next
-        if (temp):
-            new_node: Node = Node(next=temp.next, data=index)
-            temp.next = new_node
-        else:
-            self.PushBack(data = index)
-        self.sizeofLinkedList += 1
 
 if __name__ == '__main__':
     data = IslanderQueue(priority= True)
